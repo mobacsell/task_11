3eea21585afaccbc602743c99e3657183103a2f7
Aleksandr Alekseev (PHP-16) <mobacsell@yandex.ru>
2021-09-01 20:02:53 +0300

    add function getRandomAnswer()

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -23,17 +23,19 @@ document.getElementById('btnEqual').addEventListener('click', function () {
 });
 
 function startNewGame() {
-    minValue = parseInt(prompt('Минимальное знание числа для игры','0')),
-    maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+    if (!gameRun) {
+        minValue = parseInt(prompt('Минимальное знание числа для игры','0')),
+        maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
 
-    answerNumber  = Math.floor((minValue + maxValue) / 2);
-    orderNumber = 1;
-    gameRun = true;
+        answerNumber  = Math.floor((minValue + maxValue) / 2);
+        orderNumber = 1;
+        gameRun = true;
 
-    alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+        alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 
-    answerField.textContent = `Вы загадали число ${answerNumber}?`;
-    orderNumberField.innerText = orderNumber;
+        answerField.textContent = getRandomAnswer();
+        orderNumberField.innerText = orderNumber;
+    }
 }
 
 function getNextAnswer(btn) {
@@ -60,45 +62,27 @@ function getNextAnswer(btn) {
     }
 }
 
-function caseLess() {
-    if (gameRun) {
-        if (maxValue === minValue){
-            const phraseRandom = Math.round( Math.random());
-            const answerPhrase = (phraseRandom === 1) ?
-                `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
+function getRandomAnswer() {
+    let phraseRandom = Math.round(Math.random() * 3);
+    let questionPhrase;
 
-            answerField.innerText = answerPhrase;
-            gameRun = false;
-        } else {
-            maxValue = answerNumber  - 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
-        }
+    switch (phraseRandom) {
+        case 0:
+            questionPhrase = `Вы загадали число ${answerNumber}?`;
+            break;
+        case 1:
+            questionPhrase = `Мне кажется, что вы задумали число ${answerNumber}?`;
+            break;
+        case 2:
+            questionPhrase = `Неужели это число ${answerNumber}?`;
+            break;
+        case 3:
+            questionPhrase = `Это просто! Вы задумали ${answerNumber}?`;
+            break;
     }
+
+    return questionPhrase;
 }
-
-
-
-// let phraseRandom = Math.round(Math.random() * 3);
-// let questionPhrase;
-
-// switch (phraseRandom) {
-//     case 0:
-//         questionPhrase = `Вы загадали число ${answerNumber}?`;
-//         break;
-//     case 1:
-//         questionPhrase = `Мне кажется, что вы задумали число ${answerNumber}?`;
-//         break;
-//     case 2:
-//         questionPhrase = `Неужели это число ${answerNumber}?`;
-//         break;
-//     case 3:
-//         questionPhrase = `Возможно я ошибаюсь, задуманное число ${answerNumber}?`;
-//         break;
-// }
 
 
 
