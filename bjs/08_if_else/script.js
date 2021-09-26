aa24bc427d87e4a1ab13eb7010ffd250f2b8371f
Aleksandr Alekseev <mobacsell@yandex.ru>
2021-09-26 18:41:13 +0300

    finish tasks of 15 points + add comments to program code

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -17,11 +17,12 @@ document.getElementById('btnLess').addEventListener('click', getNextAnswer);
 
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun) {
-        answerField.innerText = getRandomWinMessage();
+        answerField.textContent = getRandomWinMessage();
         gameRun = false;
     }
 });
 
+//Функция startNewGame реализует функционал запуска игры и первого ответа.
 function startNewGame() {
     minValue = parseInt(prompt('Минимальное знание числа для игры','0')) || 0;
     minValue = (minValue < -999) ? -999 : minValue; 
@@ -35,9 +36,10 @@ function startNewGame() {
     alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 
     answerField.textContent = getRandomAnswer();
-    orderNumberField.innerText = orderNumber;
+    orderNumberField.textContent = orderNumber;
 }
 
+//Функция getNextAnswer реализует функционал последующего поиска числа (при нажатии кнопок <Больше> или <Меньше>)
 function getNextAnswer(btn) {
     if (gameRun) {
         if (minValue === maxValue){
@@ -46,7 +48,7 @@ function getNextAnswer(btn) {
                 `Вы загадали неправильное число!\n\u{1F914}` :
                 `Я сдаюсь..\n\u{1F92F}`;
 
-            answerField.innerText = answerPhrase;
+            answerField.textContent = answerPhrase;
             gameRun = false;
         } else {
             if (this === document.querySelector('#btnOver')) {
@@ -54,36 +56,39 @@ function getNextAnswer(btn) {
             } else {
                 maxValue = answerNumber  - 1;
             }
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            answerNumber = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            orderNumberField.textContent = orderNumber;
+            answerField.textContent = getRandomAnswer();
         }
     }
 }
 
+//Функция getRandomAnswer реализует функционал рандомного выбора ответов.
 function getRandomAnswer() {
     let phraseRandom = Math.round(Math.random() * 3);
     let questionPhrase;
 
     switch (phraseRandom) {
         case 0:
-            questionPhrase = `Вы загадали число ${answerNumber}?`;
+            questionPhrase = `Вы загадали число${getTextOfNumber(answerNumber)}?`;
             break;
         case 1:
-            questionPhrase = `Мне кажется, что вы задумали число ${answerNumber}?`;
+            questionPhrase = `Мне кажется, что вы задумали число${getTextOfNumber(answerNumber)}?`;
             break;
         case 2:
-            questionPhrase = `Неужели это число ${answerNumber}?`;
+            questionPhrase = `Неужели это число${getTextOfNumber(answerNumber)}?`;
             break;
         case 3:
-            questionPhrase = `Это просто! Вы задумали ${answerNumber}?`;
+            questionPhrase = `Это просто! Вы задумали число${getTextOfNumber(answerNumber)}?`;
             break;
     }
 
     return questionPhrase;
 }
 
+
+//Функция getRandomWinMessage реализует функционал рандомного сообщения при отгадывании числа.
 function getRandomWinMessage() {
     let phraseRandom = Math.round(Math.random() * 3);
     let questionPhrase;
@@ -106,5 +111,151 @@ function getRandomWinMessage() {
     return questionPhrase;
 }
 
+//Функция getTextOfNumber переводит числовую запись в текстовую.
+function getTextOfNumber(num) {
+    let initialNum = num;
+    let textAnswer = '';
+
+    if (num === 0) {
+        return ' ' + 0;
+    }
+
+    if (String(num)[0] === '-') {
+        textAnswer = textAnswer + ' минус';
+        num = num * (-1);
+    }
+
+    if (String(num).length > 2) {
+        switch (Math.floor(num / 100)) {
+            case 1:
+                textAnswer = textAnswer + ' сто';
+                break;
+            case 2:
+                textAnswer = textAnswer + ' двести';
+                break;
+            case 3:
+                textAnswer = textAnswer + ' триста';
+                break;
+            case 4:
+                textAnswer = textAnswer + ' четыреста';
+                break;
+            case 5:
+                textAnswer = textAnswer + ' пятьсот';
+                break;
+            case 6:
+                textAnswer = textAnswer + ' шестьсот';
+                break;
+            case 7:
+                textAnswer = textAnswer + ' семьсот';
+                break;
+            case 8:
+                textAnswer = textAnswer + ' восемьсот';
+                break;
+            case 9:
+                textAnswer = textAnswer + ' девятьсот';
+                break;
+        }
+
+        num = num % 100;
+    }
+
+    if (String(num).length > 1 && num >= 20) {
+        switch (Math.floor(num / 10)) {
+            case 2:
+                textAnswer = textAnswer + ' двадцать';
+                break;
+            case 3:
+                textAnswer = textAnswer + ' тридцать';
+                break;
+            case 4:
+                textAnswer = textAnswer + ' сорок';
+                break;
+            case 5:
+                textAnswer = textAnswer + ' пятьдесят';
+                break;
+            case 6:
+                textAnswer = textAnswer + ' шестьдесят';
+                break;
+            case 7:
+                textAnswer = textAnswer + ' семьдесят';
+                break;
+            case 8:
+                textAnswer = textAnswer + ' восемьдесят';
+                break;
+            case 9:
+                textAnswer = textAnswer + ' девяносто';
+                break;
+        }
+
+    num = num  % 10;
+    } else {
+        switch (num) {
+            case 10:
+                textAnswer = textAnswer + ' десять';
+                break;
+            case 11:
+                textAnswer = textAnswer + ' одиннадцать';
+                break;
+            case 12:
+                textAnswer = textAnswer + ' двенадцать';
+                break;
+            case 13:
+                textAnswer = textAnswer + ' тринадцать';
+                break;
+            case 14:
+                textAnswer = textAnswer + ' четырнадцать';
+                break;
+            case 15:
+                textAnswer = textAnswer + ' пятнадцать';
+                break;
+            case 16:
+                textAnswer = textAnswer + ' шестнадцать';
+                break;
+            case 17:
+                textAnswer = textAnswer + ' семнадцать';
+                break;
+            case 18:
+                textAnswer = textAnswer + ' восемнадцать';
+                break;
+            case 19:
+                textAnswer = textAnswer + ' девятнадцать';
+                break;
+        }
+    }
+
+    if (String(num).length > 0 && num < 10) {
+        switch (num) {
+            case 1:
+                textAnswer = textAnswer + ' один';
+                break;
+            case 2:
+                textAnswer = textAnswer + ' два';
+                break;
+            case 3:
+                textAnswer = textAnswer + ' три';
+                break;
+            case 4:
+                textAnswer = textAnswer + ' четыре';
+                break;
+            case 5:
+                textAnswer = textAnswer + ' пять';
+                break;
+            case 6:
+                textAnswer = textAnswer + ' шесть';
+                break;
+            case 7:
+                textAnswer = textAnswer + ' семь';
+                break;
+            case 8:
+                textAnswer = textAnswer + ' восемь';
+                break;
+            case 9:
+                textAnswer = textAnswer + ' девять';
+                break;
+        }
+    }
+
+    return (textAnswer.length > 20) ? ' ' + initialNum : textAnswer;
+}
 
 
